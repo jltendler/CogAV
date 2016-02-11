b662756a4497e383c1d6f1aaf9eb28054ca51acc
user <dama8893@colorado.edu>
2016-02-10 17:21:27 -0700

    now generates random waypoints as it moves along a given path

--- waypointGenerator.py
+++ waypointGenerator.py
@@ -1,42 +1,51 @@
 import random
 
-start_Long = 50
-end_Long = 50
-
-start_Lat = 50
-end_Lat = 50
-
+latPath = [50, 55, 60, 65, 70, 65, 50, 45, 40]
+lonPath = [40, 45, 50, 55, 60, 55, 50, 35, 30]
 """
-This script is a basic attempt at generating random GPS coordinates defined within a certain range
-TODO: Mess with the numbers to make them generate more acurtely
-TODO: Make it more abstract to be able to work with live coordinate list
+This will generate random points around each position as car moves from start of path
+to the end of path in a step wise motion.
+TODO: use the outputed text file: coordinates.txt to read into routeCalc.py
+and figure out how to make it useful in our route calculation
+TODO: fit random GPS generation to a poly-fit curve path or fastest route
 """
-def gen_random_waypoints_long(low, high):
-	longitude_list = []
-	for i in range(100):
-		while low > 0 and high <= 100:
-			i = random.uniform(low, high) #Generates a random float imbetween range low and high
-			longitude_list.append(i)
-			low -= 1
-			high +=1
-	return longitude_list
-
-def gen_random_waypoints_lat(low, high):
+def gen_random_waypoints_onPath():
 	lattitude_list = []
-	for i in range(100):
-		while low > 0 and high <= 100:
-			i = random.uniform(low, high) #Generates a random float imbetween range low and high
-			lattitude_list.append(i)
-			low -= 1
-			high += 1
-	return lattitude_list
+	longitude_list = []
+	curLat = latPath[0]
+	curLon = lonPath[0]
 
-lon = gen_random_waypoints_long(start_Long, end_Long)
-lat = gen_random_waypoints_lat(start_Lat, end_Lat)
+	i = 0
+	while not ((curLat == latPath[len(latPath) - 1]) and (curLon == lonPath[len(lonPath) - 1])): # Not at the end
+		if ((curLat == latPath[i]) and (curLon == lonPath[i])): # At point in path
+			i += 1
+			while latPath[i] - curLat > 0:
+				curLat += 1
+				randLat = float(curLat) + random.uniform(0, 10) # add random float between 0 and 10 to current position
+				lattitude_list.append(randLat)
+			
+			while latPath[i] - curLat < 0:
+				curLat -= 1
+				randLat = float(curLat) + random.uniform(0, 10) # add random float between 0 and 10 to current position
+				lattitude_list.append(randLat)
+			
+			while lonPath[i] - curLon > 0:
+				curLon += 1
+				randLon = float(curLon) + random.uniform(0, 10) # add random float between 0 and 10 to current position
+				longitude_list.append(randLon)
+			
+			while lonPath[i] - curLon < 0:
+				curLon -= 1
+				randLon = float(curLon) + random.uniform(0, 10) # add random float between 0 and 10 to current position
+				longitude_list.append(randLon)
+	
+	return lattitude_list, longitude_list
 
-coordinate_file = open("coordinates.txt", 'w')
-for x in range(len(lon)):
-	coordinate_file.write(str((lon[x], lat[x])) + "\n")
+def main():
+	(lat, lon) = gen_random_waypoints_onPath()
+	coordinate_file = open("coordinates.txt", 'w')
+	for x in range(len(lon)):
+		coordinate_file.write(str((lat[x], lon[x])) + '\n')
+	coordinate_file.close()
 
-coordinate_file.close()
-
+main()
