85e65a2ec1dab8b2e62544b1774c8929cfc257a4
user <dama8893@colorado.edu>
2016-03-06 17:28:00 -0700

    now calculating off waypoint path

--- routeCalc.py
+++ routeCalc.py
@@ -6,11 +6,21 @@ import decimal
 
 class routeCalc:
     def __init__(self, curX, curY):
-        self.curX = curX
-        self.curY = curY
+        self.curX = decimal.Decimal(curX)
+        self.curY = decimal.Decimal(curY)
 
-    pathX = [0, 3, 4, 6, 4, 9, 2]
-    pathY = [0, 3, 6, 2, 0, 1, 8]
+    pathX = np.array([40.071374, 40.071258, 40.070755, 40.070976, 40.071331])
+    pathY = np.array([-105.229788, -105.230026, -105.229717, -105.229191, -105.229466])
+    pathX=np.array([decimal.Decimal(abc) for abc in pathX])
+    pathY=np.array([decimal.Decimal(abc) for abc in pathY])
+    for x in range(len(pathX)):
+        pathX[x] = decimal.Decimal(pathX[x])
+        pathX[x]+=0
+
+    for x in range(len(pathY)):
+        pathY[x] = decimal.Decimal(pathY[x])
+        pathY[x]+=0
+
 
     def testWaypoint(self):
         self.waypointCount += 1
@@ -41,21 +51,24 @@ class routeCalc:
 			set PWM(0,0,375) go straight
 	"""
 		
+    increment = decimal.Decimal(0.0001) #Increment distance
+    threshold = decimal.Decimal(0.0001) #threshold to check if within certain distance
+
     def moveNorth(self):
         print "moving north"
-        self.curY += 1
+        self.curY += self.increment
 
     def moveSouth(self):
         print "moving south"
-        self.curY -= 1
+        self.curY -= self.increment
 
     def moveEast(self):
         print "moving east"
-        self.curX += 1
+        self.curX += self.increment
 
     def moveWest(self):
         print "moving west"
-        self.curX -= 1
+        self.curX -= self.increment
 
     def findAngle(self):
 		#Needs to compare pathX/pathY to curX/curY
@@ -63,17 +76,19 @@ class routeCalc:
         arctanList = []
         for i in range(len(self.pathX)):
             if i is 0:
-                y = np.arctan2(self.pathY[i], self.pathX[i])
+                y = np.arctan2(float(self.pathY[i]), float(self.pathX[i]))
                 if y < 0:
-					acctanList.append((2*np.pi)+y)
+					arctanList.append((2*np.pi)+y)
                 else:
                     arctanList.append(y)
             else:
-				x = np.arctan2(self.pathY[i]-self.pathY[i-1], self.pathX[i]-self.pathX[i-1])
-				if x < 0:
-					arctanList.append((2*np.pi)+(x))
-				else:
-					arctanList.append(x)
+                x1=self.pathY[i]-self.pathY[i-1]
+                x2=self.pathX[i]-self.pathX[i-1]
+                x = np.arctan2(float(x1), float(x2))
+                if x < 0:
+                    arctanList.append((2*np.pi)+(x))
+                else:
+                    arctanList.append(x)
 
         arctanList = np.degrees(arctanList)
         return arctanList
@@ -81,8 +96,9 @@ class routeCalc:
     def findAbsAngle(self):
         #Calculating the absolute arctans of path
         absArctanList = []
+        print ("Finding abs angle")
         for i in range(len(self.pathX)):
-                absArctanList.append(np.arctan2(self.pathY[i], self.pathX[i]))
+                absArctanList.append(np.arctan2(float(self.pathY[i]), float(self.pathX[i])))
 
         absArctanList = np.degrees(absArctanList)
         return absArctanList
@@ -104,43 +120,51 @@ class routeCalc:
                 absDistList.append(np.sqrt(abs(np.square(self.pathX[i]) + np.square(self.pathY[i]))))
         return absDistList
         
+        
     def checkIfFinished(self):
-        return ((self.curX == self.pathX[len(self.pathX)-1]) and (self.curY == self.pathY[len(self.pathY)-1]))
+        #Checks if current coordinates are within threshold of the specified end of path
+        return (self.checkIfInThreshold(self.curX, self.pathX[len(self.pathX)-1]) and self.checkIfInThreshold(self.curY, self.pathY[len(self.pathY)-1]))
         
     def checkIfAtPair(self, i):
-        #Checks if current coordinates are at the specified pathX/pathY coordinates
-        return ((self.curX == self.pathX[i]) and (self.curY == self.pathY[i]))
+        #Checks if current coordinates are within threshold of specified point in path
+        return (self.checkIfInThreshold(self.curX,self.pathX[i]) and self.checkIfInThreshold(self.curY,self.pathY[i]))
+        
+        
+    def checkIfInThreshold(self, a, b):
+        if(abs(decimal.Decimal(b) - decimal.Decimal(a)) < self.threshold):
+            return True
+        else:
+            return False
 
 def main():
-    route = routeCalc(0,0)
+    route = routeCalc(decimal.Decimal(40.071374),decimal.Decimal(-105.229788))
     i = 0
     xRoute = []
     yRoute = []
-    route = routeCalc(0,0)
     route.waypointCount = 0
 
     while not (route.checkIfFinished()):	#Not done
         if (route.checkIfAtPair(i)):  #At current goal
             i += 1
-            while route.pathX[i] - route.curX > 0:
+            while route.pathX[i] - route.curX > 0.0:
                 print (route.curX, route.curY)
                 route.moveEast()
                 xRoute.append(route.curX)
                 yRoute.append(route.curY)
 
-            while route.pathX[i] - route.curX < 0:
+            while route.pathX[i] - route.curX < 0.0:
                 print (route.curX, route.curY)
                 route.moveWest()
                 xRoute.append(route.curX)
                 yRoute.append(route.curY)
 
-            while route.pathY[i] - route.curY > 0:
+            while route.pathY[i] - route.curY > 0.0:
                 print (route.curX, route.curY)
                 route.moveNorth()
                 xRoute.append(route.curX)
                 yRoute.append(route.curY)
 
-            while route.pathY[i] - route.curY < 0:
+            while route.pathY[i] - route.curY < 0.0:
                 print (route.curX, route.curY)
                 route.moveSouth()
                 xRoute.append(route.curX)
@@ -172,9 +196,10 @@ def main():
 
     angles = np.array(absAngles) 
     lengths = np.array(absDistances) 
-
     ax = plt.subplot(111, projection='polar')
     ax.plot(angles * np.pi / 180, lengths, 'r', linewidth=3)
+    ax.set_rmax(112.602)
+    ax.set_rmin(112.600)
     ax.grid(True)
     ax.set_title("Simulating vehicle movement", va='bottom')
     plt.show()
