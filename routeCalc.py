a88de54a5a4309a34a901c72037279464277df13
Patrick Andresen <patrick.andresen@colorado.edu>
2016-04-22 20:38:17 +0000

    both sensors streaming live data

--- routeCalc.py
+++ routeCalc.py
@@ -9,11 +9,85 @@ import os
 from gps import *
 import threading
 from Adafruit_BNO055 import BNO055
+import RPi.GPIO as GPIO
 
 gpsd = None
 #Initialize serial port for Compass
 bno = BNO055.BNO055(serial_port='/dev/ttyUSB0', rst=18)
 
+leftSensor = None
+rightSensor = None
+
+
+class rangeFinder(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self) #create separate thread
+
+  def reading(self, sensor):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    if sensor == 0: # left sensor
+      # point the software to the GPIO pins the sensor is using
+      # change these values to the pins you are using
+      # GPIO output = the pin that's connected to "Trig" on the sensor
+      # GPIO input = the pin that's connected to "Echo" on the sensor
+
+      #left sensor
+      GPIO.setup(21,GPIO.OUT)
+      GPIO.setup(20,GPIO.IN)
+      GPIO.output(21, GPIO.LOW)
+
+
+      # start the pulse on the GPIO pin 
+      # change this value to the pin you are using
+      # GPIO output = the pin that's connected to "Trig" on the sensor
+      GPIO.output(21, True) #left sensor
+
+      # wait 10 micro seconds (this is 0.00001 seconds) so the pulse
+      # length is 10Us as the sensor expects
+      time.sleep(0.00001)
+
+      GPIO.output(21, False) #left sensor
+      while GPIO.input(20) == 0: #left sensor
+        self.signaloff = time.time()
+
+      while GPIO.input(20) == 1:
+        self.signalon = time.time()
+      
+      self.timepassed = self.signalon - self.signaloff
+      self.distance = self.timepassed * 21000
+
+      #return self.distance
+      #GPIO.cleanup()
+      global leftSensor
+      leftSensor = self.distance
+
+    if sensor == 1: # right sensor
+       #right sensor
+      #21 = 19, 20 = 16
+      GPIO.setup(19,GPIO.OUT)
+      GPIO.setup(16,GPIO.IN)
+      GPIO.output(19, GPIO.LOW)
+
+      GPIO.output(19, True) #right sensor
+      # wait 10 micro seconds (this is 0.00001 seconds) so the pulse
+      # length is 10Us as the sensor expects
+      time.sleep(0.00001)
+      GPIO.output(19, False) #right sensor
+
+      while GPIO.input(16) == 0: #right sensor
+        self.signaloff_R = time.time()
+
+      while GPIO.input(16) == 1:
+        self.signalon_R = time.time()
+      self.timepassed_R = self.signalon_R - self.signaloff_R
+      self.distance_R = self.timepassed_R * 21000
+      global rightSensor
+      rightSensor = self.distance_R
+      
+
+      
+      
 
 class GpsPoller(threading.Thread):
   def __init__(self):
@@ -141,12 +215,12 @@ class routeCalc:
         a = np.degrees(a)
         if a <= 90:
           compass_angle = 90 - a
-          print "arctan < 90"
+          #print "arctan < 90"
         else:
           compass_angle = 360 - (a - 90)
-          print "arctan > 90"
+          #print "arctan > 90"
           
-        print ("angle of direction=", compass_angle)
+        #print ("angle of direction=", compass_angle)
         temp = (360 - self.pullHeading()) + compass_angle
         temp = temp % 360
         temp2 = temp - 360
@@ -235,17 +309,20 @@ class routeCalc:
         angle = self.findCurrAngle()
         turnPWM = self.calcTurn()
         speedPWM = self.calcSpeed()
-        print ("curX:", self.curX)
-        print ("curY:", self.curY)
-        print ("angle is equal to:", angle) 
-        print ("setting turn PWM to:", int(turnPWM)) # Also in calcTurn()
-        print ("setting speed PWM to:", int(speedPWM)) # Also in calcSpeed()
+        #print ("curX:", self.curX)
+        #print ("curY:", self.curY)
+        #print ("angle is equal to:", angle) 
+        #print ("setting turn PWM to:", int(turnPWM)) # Also in calcTurn()
+        #print ("setting speed PWM to:", int(speedPWM)) # Also in calcSpeed()
         self.pwm.setPWM(0,0,int(turnPWM)) #--> moved this call into calcTurn()
         self.pwm.setPWM(1,0,int(speedPWM)) #--> moved this call into calcSpeed()
-        print("current goal:" , (self.pathX[self.waypointCounter], self.pathY[self.waypointCounter]))
-        print ("heading = ", c_pass.heading)
+        #print("current goal:" , (self.pathX[self.waypointCounter], self.pathY[self.waypointCounter]))
+        #print ("heading = ", c_pass.heading)
         # make sure sleep commented out when running
         # can turn back on to see data
+        findRange.reading(0)
+        findRange.reading(1)
+        print("Left: "+ str(leftSensor) +" Right: "+str(rightSensor))
        # time.sleep(1)
         
         
@@ -310,6 +387,7 @@ if __name__ == '__main__':
     gpsp = GpsPoller()
     gpsp.start()
     c_pass = compass()
+    findRange = rangeFinder()
 
 main()
 
